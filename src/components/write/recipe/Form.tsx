9a64cf93a73d52ad39edb6f72453b56cea65c9b0
Koushik <programmer.koushik@gmail.com>
2023-11-21 21:29:06 +0530

    delete

--- src/components/write/recipe/Form.tsx
+++
@@ -1,9 +0,0 @@
-import Recipe from "./Recipe";
-
-export default function Form() {
-  return (
-    <section>
-      <Recipe />
-    </section>
-  );
-}
--- src/components/write/step/Step.tsx
+++
@@ -1,9 +0,0 @@
-import StepForm from "./StepForm";
-
-export default function Step() {
-  return (
-    <section>
-      <StepForm />
-    </section>
-  );
-}

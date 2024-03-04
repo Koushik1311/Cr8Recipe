1f84484d7ef7b60e6a3f4b8dbfaedb267aae63fd
Koushik <programmer.koushik@gmail.com>
2024-03-04 23:22:50 +0530

    update color

--- src/components/Shared/Btn.tsx
+++ src/components/Shared/Btn.tsx
@@ -7,7 +7,7 @@ type BtnProps = {
 
 export default function Btn({ children, goTo }: BtnProps) {
   return (
-    <button className="bg-slate-950 text-white py-[0.6rem] px-[1rem] border-none rounded-full text-sm font-semibold">
+    <button className="hover:bg-[#3C6E38] bg-[#CAF0C7] text-[#3C6E38] hover:text-white py-[0.6rem] px-[1.5rem] border-none rounded-full text-sm font-semibold transition-all">
       <Link href={goTo}>{children}</Link>
     </button>
   );

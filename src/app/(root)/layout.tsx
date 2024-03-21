b936e3f8681e9e4c8ff4684ac779ba70d0ec0f48
Koushik <programmer.koushik@gmail.com>
2024-03-21 23:01:05 +0530

    home metadata

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from "next";
+
 import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
+
+export const metadata: Metadata = {
+  title: "Cr8Recipe",
+  description: "Next generation Recipe Sharing app",
+};
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (

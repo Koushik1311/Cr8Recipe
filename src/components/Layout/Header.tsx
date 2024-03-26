40be0f672ca05f6987225ce7c86b376569e2dd7b
Koushik <programmer.koushik@gmail.com>
2024-03-26 23:01:20 +0530

    some changes

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,18 @@
-import NavBar from "./NavBar";
+import BigDeviceNavBar from "./BigDeviceNavBar";
+import SmallDeviceNavBar from "./SmallDeviceNavBar";
 
 export default function Header() {
   return (
-    <section className="hidden lg:block">
-      <NavBar />
-    </section>
+    <>
+      {/* Small Device NavBar */}
+      <section className="lg:hidden">
+        <SmallDeviceNavBar />
+      </section>
+
+      {/* Big Device NavBar */}
+      <section className="hidden lg:block">
+        <BigDeviceNavBar />
+      </section>
+    </>
   );
 }

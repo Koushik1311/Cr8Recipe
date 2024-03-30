31ffdc54e3e55c68f318a8dc856938400a0f8862
Koushik <programmer.koushik@gmail.com>
2024-03-30 23:59:11 +0530

    heading

--- src/components/Recipe/RecipeList.tsx
+++ src/components/Recipe/RecipeList.tsx
@@ -1,33 +1,18 @@
-import { GiChefToque } from "react-icons/gi";
-import Image from "next/image";
-import Link from "next/link";
-
 export default function RecipeList() {
   return (
     <div className="w-screen flex items-center justify-center mx-auto relative">
+      <div className="flex flex-col items-center max-w-[1100px]">
+        {/* Heading */}
+        <h1 className="text-6xl text-center">
+          Explore the World&apos;s Most Innovative Culinary Minds & Creatives
+        </h1>
+        <p className="text-sm text-center">
+          Cr8Recipe: Where Culinary Creativity Thrives - Share and Discover
+          Delicious Recipes from Top Creators Worldwide
+        </p>
+      </div>
+
       {/* All Recipes, New Recipes, Trending Recipes, Featured Recipes */}
-      <div className="absolute lg:hidden">
-        <GiChefToque />
-      </div>
-      <div className="w-[1200px] border-2">
-        <div className="flex">
-          <div className="flex flex-col text-sm font-semibold text-gray-400">
-            <Link href="#" className="global-filter">
-              All Recipes
-            </Link>
-            <Link href="#" className="global-filter">
-              New Recipes
-            </Link>
-            <Link href="#" className="global-filter">
-              Trending Recipes
-            </Link>
-            <Link href="#" className="global-filter">
-              Featured Recipes
-            </Link>
-          </div>
-          <div></div>
-        </div>
-      </div>
     </div>
   );
 }

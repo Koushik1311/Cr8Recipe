7dfa0a8db3b506332f7bf5f3d1c60f1bc32b5006
Koushik <programmer.koushik@gmail.com>
2024-04-02 22:32:14 +0530

    recipe list

--- src/components/Recipe/RecipeList.tsx
+++ src/components/Recipe/RecipeList.tsx
@@ -1,18 +1,46 @@
+import { recipeData } from "@/data/recipe/data";
+import Image from "next/image";
+import { IoMdStar, IoMdStarHalf } from "react-icons/io";
+
 export default function RecipeList() {
   return (
-    <div className="w-screen flex items-center justify-center mx-auto relative">
-      <div className="flex flex-col items-center max-w-[1100px]">
-        {/* Heading */}
-        <h1 className="text-6xl text-center">
-          Explore the World&apos;s Most Innovative Culinary Minds & Creatives
-        </h1>
-        <p className="text-sm text-center">
-          Cr8Recipe: Where Culinary Creativity Thrives - Share and Discover
-          Delicious Recipes from Top Creators Worldwide
-        </p>
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-9">
+        {recipeData.map((recipe, index) => (
+          <div key={index} className="rounded-lg">
+            <Image
+              src={recipe.image}
+              width={290}
+              height={290}
+              quality={100}
+              priority={true}
+              alt={recipe.title}
+              className="w-[260px] h-[260px] object-cover rounded-md"
+            />
+            <h3 className="mt-4 text-base font-normal text-gray-700">
+              {recipe.title}
+            </h3>
+            <p className="text-base font-light text-gray-500">
+              {recipe.author}
+            </p>
+            <div className="flex mt-2 text-2xl">
+              {[...Array(5)].map((_, i) =>
+                i < Math.floor(recipe.ratting) ? (
+                  <IoMdStar key={i} className={`text-2xl text-orange-500`} />
+                ) : i === Math.floor(recipe.ratting) &&
+                  recipe.ratting % 1 !== 0 ? (
+                  <IoMdStarHalf
+                    key={i}
+                    className={`text-2xl text-orange-500`}
+                  />
+                ) : (
+                  <IoMdStar key={i} className={`text-2xl text-gray-300`} />
+                )
+              )}
+            </div>
+          </div>
+        ))}
       </div>
-
-      {/* All Recipes, New Recipes, Trending Recipes, Featured Recipes */}
     </div>
   );
 }

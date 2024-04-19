0995f9f08e4174efa9bdeaa5dce669d080b16dd5
Koushik <programmer.koushik@gmail.com>
2024-04-19 21:09:45 +0530

    fix button problem

--- src/components/Shared/Btn.tsx
+++ src/components/Shared/Btn.tsx
@@ -7,8 +7,10 @@ type BtnProps = {
 
 export default function Btn({ children, goTo }: BtnProps) {
   return (
-    <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white py-[0.6rem] px-[1.5rem] rounded-full text-sm font-semibold transition-all">
-      <Link href={goTo}>{children}</Link>
-    </button>
+    <Link href={goTo}>
+      <button className="border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white py-[0.6rem] px-[1.5rem] rounded-full text-sm font-semibold transition-all">
+        {children}
+      </button>
+    </Link>
   );
 }
